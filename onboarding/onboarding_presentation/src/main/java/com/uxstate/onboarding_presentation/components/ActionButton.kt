63cef25762b146ba4c57512b381d6aabbc3918a4
Antony <vontonnie@gmail.com>
2022-02-15 19:23:39 +0300

    Add Global Action Button

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/components/ActionButton.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/components/ActionButton.kt
@@ -1,9 +1,15 @@
 package com.uxstate.onboarding_presentation.components
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import com.uxstate.core_ui.LocalSpacing
 
 @Composable
 fun ActionButton(
@@ -14,4 +20,21 @@ fun ActionButton(
     textStyle: TextStyle = MaterialTheme.typography.button
 ) {
 
+
+    Button(
+        onClick = onclick,
+        modifier = modifier,
+        enabled = isEnabled,
+        shape = RoundedCornerShape(100.dp),
+    ) {
+
+        Text(
+            text = text,
+            style = textStyle,
+            color = MaterialTheme.colors.onPrimary,
+            modifier = Modifier.padding(
+                LocalSpacing.current.spaceExtraSmall
+            )
+        )
+    }
 }
