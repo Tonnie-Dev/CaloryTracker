245eef9c54a2b9946bddf0ef34209fb01334ddf3
Antony <vontonnie@gmail.com>
2022-02-15 07:57:43 +0300

    Copy-Paste Lib Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,7 @@ buildscript {
         classpath(Build.androidBuildTools)
         classpath(Build.hiltAndroidGradlePlugin)
         classpath(Build.kotlinGradlePlugin)
-        /*classpath ("com.android.tools.build:gradle:7.1.1")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")*/
+
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
