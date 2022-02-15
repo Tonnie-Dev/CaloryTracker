2da351da215ae885b1b0b92d6cab5fa8c3378657
Antony <vontonnie@gmail.com>
2022-02-15 18:56:55 +0300

    Add Dimens

--- core-ui/src/main/java/com/uxstate/core_ui/Dimensions.kt
+++ core-ui/src/main/java/com/uxstate/core_ui/Dimensions.kt
@@ -1,3 +1,13 @@
 package com.uxstate.core_ui
 
-data class Dimensions(val)
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val default: Dp = 0.dp,
+    val spaceExtraSmall: Dp = 4.dp,
+    val spaceSmall: Dp = 8.dp,
+    val spaceMedium: Dp = 16.dp,
+    val spaceLarge: Dp = 32.dp,
+val spaceExtraLarge:Dp = 64.dp
+)
