d0cf0f6a66515043b816dba2f222a99bed3a8ff5
Antony <vontonnie@gmail.com>
2022-02-15 18:34:49 +0300

    Add Welcome Text

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/welcome/WelcomeScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -1,29 +1,32 @@
 package com.uxstate.onboarding_presentation.welcome
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.uxstate.core.R
 
 @Composable
 fun WelcomeScreen() {
-   Column(
-       modifier = Modifier.fillMaxSize(),
-       horizontalAlignment = Alignment.CenterHorizontally,
-       verticalArrangement = Arrangement.Center
-   ) {
-       
-       Text(text = "")
-   }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = stringResource(id = R.string.welcome_text),
+            style = MaterialTheme.typography.h1,
+            textAlign = TextAlign.Center
+        )
+    }
 }
 
 
