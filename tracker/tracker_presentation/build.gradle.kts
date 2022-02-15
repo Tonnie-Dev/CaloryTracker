7efe509f2a27051463435ec006304e5a506d5ff2
Antony <vontonnie@gmail.com>
2022-02-15 08:33:36 +0300

    Add specific dependencies for the tracker_presentation module

--- tracker/tracker_presentation/build.gradle.kts
+++ tracker/tracker_presentation/build.gradle.kts
@@ -2,4 +2,10 @@ apply{
     from("$rootDir/base-module.gradle")
 }
 
-dependencies{}
+dependencies{
+
+    "implementation"(project(Modules.core))
+    "implementation"(project(Modules.trackerDomain))
+
+    "implementation"(Coil.coilCompose)
+}
