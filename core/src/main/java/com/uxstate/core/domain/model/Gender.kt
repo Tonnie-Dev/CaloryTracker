5f77210b98218fc561b68b02994e66ba54b6785b
Antony <vontonnie@gmail.com>
2022-02-16 08:32:20 +0300

    Add CompanionObject to Gender sealed class

--- core/src/main/java/com/uxstate/core/domain/model/Gender.kt
+++ core/src/main/java/com/uxstate/core/domain/model/Gender.kt
@@ -1,8 +1,22 @@
 package com.uxstate.core.domain.model
 
-sealed class Gender{
+sealed class Gender( val name:String){
+
+/*the name arg helps us save entry on SharedPrefs as we
+    cannot save sealed class in SharedPrefs*/
+    object Male:Gender(name ="male")
+    object Female:Gender(name ="female")
 
 
-    object Male:Gender()
-    object Female:Gender()
+    //easily take a string and return a gender  object
+    companion object{
+        fun fromString(name: String) :Gender {
+
+            return when (name){
+
+                "male" -> Male
+                else -> Female
+            }
+        }
+    }
 }
