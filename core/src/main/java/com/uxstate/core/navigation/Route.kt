f3370c857b07dd04fa94ec1a327e86b7313b0a65
Antony <vontonnie@gmail.com>
2022-02-16 06:48:49 +0300

    Add Route Object for global app destinations

---
+++ core/src/main/java/com/uxstate/core/navigation/Route.kt
@@ -0,0 +1,19 @@
+package com.uxstate.core.navigation
+
+/*contains all global nav destinations*/
+object Route {
+
+    //onboarding destinations
+    const val WELCOME = "welcome"
+    const val AGE = "age"
+    const val GENDER = "gender"
+    const val HEIGHT = "height"
+    const val NUTRIENT_GOAL = "nutrient_goal"
+    const val ACTIVITY = "activity"
+    const val GOAL = "goal"
+
+
+    //tracker screen
+    const val TRACKER_OVERVIEW = "tracker_overview"
+    const val SEARCH = "search"
+}
