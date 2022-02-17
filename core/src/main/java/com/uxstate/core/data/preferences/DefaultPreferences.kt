06a965e96aee3f31609545fe5f3377b35d950ace
Antony <vontonnie@gmail.com>
2022-02-17 07:34:05 +0300

    Implement Preferences on the DefaultPreferences class

--- core/src/main/java/com/uxstate/core/data/preferences/DefaultPreferences.kt
+++ core/src/main/java/com/uxstate/core/data/preferences/DefaultPreferences.kt
@@ -11,7 +11,7 @@ class DefaultPreferences(private val sharedPrefs: SharedPreferences) : Preferenc
     override fun saveGender(gender: Gender) {
 
         sharedPrefs.edit()
-                .putString("gender", gender.name)
+                .putString(Preferences.KEY_GENDER, gender.name)
                 .apply()
 
     }
@@ -19,53 +19,82 @@ class DefaultPreferences(private val sharedPrefs: SharedPreferences) : Preferenc
     override fun saveAge(age: Int) {
 
         sharedPrefs.edit()
-                .putInt("age", age)
+                .putInt(Preferences.KEY_AGE, age)
                 .apply()
     }
 
     override fun saveWeight(weight: Float) {
 
-        sharedPrefs.edit().putFloat("weight", weight).apply()
+        sharedPrefs.edit()
+                .putFloat(Preferences.KEY_WEIGHT, weight)
+                .apply()
     }
 
     override fun saveHeight(height: Int) {
-        sharedPrefs.edit().putInt("height", height).apply()
+        sharedPrefs.edit()
+                .putInt(Preferences.KEY_HEIGHT, height)
+                .apply()
     }
 
     override fun saveActivityLevel(level: ActivityLevel) {
 
-        sharedPrefs.edit().putString("level", level.level ).apply()
+        sharedPrefs.edit()
+                .putString(Preferences.KEY_ACTIVITY_LEVEL, level.level)
+                .apply()
     }
 
     override fun saveGoalType(goalType: GoalType) {
 
-        sharedPrefs.edit().putString("goal",goalType.goal).apply()
+        sharedPrefs.edit()
+                .putString(Preferences.KEY_GOAL_TYPE, goalType.goal)
+                .apply()
     }
 
     override fun saveCarbRation(ratio: Float) {
-     sharedPrefs.edit().putFloat("carbRatio", ratio).apply()
+        sharedPrefs.edit()
+                .putFloat(Preferences.KEY_CARB_RATIO, ratio)
+                .apply()
     }
 
     override fun saveProteinRatio(ratio: Float) {
 
-        sharedPrefs.edit().putFloat("proteinRatio", ratio).apply()
+        sharedPrefs.edit()
+                .putFloat(Preferences.KEY_PROTEIN_RATIO, ratio)
+                .apply()
     }
 
     override fun saveFatRatio(ratio: Float) {
-      sharedPrefs.edit().putFloat("fatRatio", ratio ).apply()
+        sharedPrefs.edit()
+                .putFloat(Preferences.KEY_PROTEIN_RATIO, ratio)
+                .apply()
     }
 
     override fun loadUserInfo(): UserInfo {
-     return UserInfo(
-         gender =sharedPrefs.getString("gender").fr,
-         age = 0,
-         weight = 0.0f,
-         height = 0,
-         activityLevel =,
-         goalType =,
-         carbRatio = 0.0f,
-         proteinRatio = 0.0f,
-         fatRatio = 0.0f
-     )
+
+        val genderString = sharedPrefs.getString(Preferences.KEY_GENDER, null)
+        val activityLevelString = sharedPrefs.getString(Preferences.KEY_ACTIVITY_LEVEL, null)
+        val goalTypeString = sharedPrefs.getString(Preferences.KEY_GOAL_TYPE, null)
+        val age = sharedPrefs.getInt(Preferences.KEY_AGE, -1)
+        val weight = sharedPrefs.getFloat(Preferences.KEY_WEIGHT, -1f)
+        val height = sharedPrefs.getInt(Preferences.KEY_HEIGHT, -1)
+        val carbRatio = sharedPrefs.getFloat(Preferences.KEY_CARB_RATIO, -1f)
+        val proteinRatio = sharedPrefs.getFloat(Preferences.KEY_PROTEIN_RATIO, -1f)
+        val fatRatio = sharedPrefs.getFloat(Preferences.KEY_FAT_RATIO, -1f)
+
+
+
+        return UserInfo(
+
+
+            gender = Gender.fromString(genderString ?: "male"),
+            age = age,
+            weight = weight,
+            height = height,
+            activityLevel = ActivityLevel.fromString(activityLevelString?: "medium"),
+            goalType = GoalType.fromString(goalTypeString ?:"keep_weight"),
+            carbRatio = carbRatio,
+            proteinRatio = proteinRatio,
+            fatRatio = fatRatio
+        )
     }
 }
