f062a5f926ebf2031beff5f6f9defad101c8a0b7
Antony <vontonnie@gmail.com>
2022-02-19 08:49:38 +0300

    Add onNextClick viewModel function

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/gender/GenderViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/gender/GenderViewModel.kt
@@ -5,12 +5,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.core.domain.model.Gender
 import com.uxstate.core.domain.preferences.Preferences
+import com.uxstate.core.navigation.Route
 import com.uxstate.core.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -24,7 +27,7 @@ class GenderViewModel @Inject constructor(private val prefs: Preferences) :
      from within the ViewModel - we should never change the vw state
      from within our composables*/
     var selectedGender by mutableStateOf<Gender>(Gender.Male)
-    private set
+        private set
 
     /*State flows upwards to the ViewModel and Events come down*/
 
@@ -32,14 +35,33 @@ class GenderViewModel @Inject constructor(private val prefs: Preferences) :
     * Navigation, snackbar, confirm successful validation */
 
     //ViewModel version
-   private val _uiEvent = Channel<UIEvent>()
+    private val _uiEvent = Channel<UIEvent>()
 
     //public uiEvent version - triggered once for every event sent to the UI
     val uiEvent = _uiEvent.receiveAsFlow()
 
 
-    fun onGenderClick(gender: Gender){
+    fun onGenderClick(gender: Gender) {
 
         selectedGender = gender
+    }
+
+
+    fun onNextClick(): Unit {
+
+        /*sending an event into a channel is a suspending operation*/
+
+        viewModelScope.launch {
+
+            //save gender into prefs
+
+            prefs.saveGender(selectedGender)
+
+
+            //navigate
+            _uiEvent.send(UIEvent.Navigate(route = Route.AGE))
+        }
+
+
     }
 }
