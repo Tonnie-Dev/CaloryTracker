d5759a51858da12a49fea86a4cfb5441dcb2d16a
Antony <vontonnie@gmail.com>
2022-02-21 20:38:26 +0300

    Add Scaffold in the MainActivity

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -3,6 +3,10 @@ package com.plcoding.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -27,6 +31,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             CaloryTrackerTheme {
                 val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                
+                Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+                    
+                }
                  NavHost(
                     navController = navController,
                     startDestination = Route.WELCOME,
