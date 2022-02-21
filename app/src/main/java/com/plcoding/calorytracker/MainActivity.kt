91f711acfb62d5b042864d632bc13e03c326b9c8
Antony <vontonnie@gmail.com>
2022-02-21 20:39:10 +0300

    Move NavHost into the scaffold

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -34,49 +34,49 @@ class MainActivity : ComponentActivity() {
                 val scaffoldState = rememberScaffoldState()
                 
                 Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
-                    
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME,
+                        builder = {
+
+                            composable(route = Route.WELCOME, content = {
+
+                                WelcomeScreen(onNavigate = navController::navigate)
+                            })
+                            composable(route = Route.AGE, content = {
+
+                                AgeScreen(onNavigate = navController::navigate)
+
+                            })
+                            composable(route = Route.GENDER, content = {
+
+                                GenderScreen(onNavigate = navController::navigate)
+                            })
+
+                            composable(route = Route.GOAL, content = {
+
+                            })
+
+                            composable(route = Route.HEIGHT, content = {
+
+                            })
+
+                            composable(route = Route.NUTRIENT_GOAL, content = {
+
+                            })
+                            composable(route = Route.SEARCH, content = {
+
+                            })
+                            composable(route = Route.TRACKER_OVERVIEW, content = {
+
+                            })
+
+                            composable(route = Route.ACTIVITY, content = {
+
+                            })
+                        })
                 }
-                 NavHost(
-                    navController = navController,
-                    startDestination = Route.WELCOME,
-                    builder = {
 
-                        composable(route = Route.WELCOME, content = {
-
-                            WelcomeScreen(onNavigate = navController::navigate)
-                        })
-                        composable(route = Route.AGE, content = {
-
-                            AgeScreen(onNavigate = navController::navigate)
-
-                        })
-                        composable(route = Route.GENDER, content = {
-
-                            GenderScreen(onNavigate = navController::navigate)
-                        })
-
-                        composable(route = Route.GOAL, content = {
-
-                        })
-
-                        composable(route = Route.HEIGHT, content = {
-
-                        })
-
-                        composable(route = Route.NUTRIENT_GOAL, content = {
-
-                        })
-                        composable(route = Route.SEARCH, content = {
-
-                        })
-                        composable(route = Route.TRACKER_OVERVIEW, content = {
-
-                        })
-
-                        composable(route = Route.ACTIVITY, content = {
-
-                        })
-                    })
             }
         }
     }
