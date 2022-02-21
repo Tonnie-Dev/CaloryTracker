74566f5e04c2af4c539569f971caed2574b7ade8
Antony <vontonnie@gmail.com>
2022-02-21 08:14:55 +0300

    Add onAgeEnter on AgeViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/age/AgeViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/age/AgeViewModel.kt
@@ -1,6 +1,8 @@
 package com.uxstate.onboarding_presentation.age
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.uxstate.core.domain.preferences.Preferences
@@ -17,7 +19,7 @@ class AgeViewModel @Inject constructor(private val prefs: Preferences) :
     ViewModel() {
 
 
-    var age = mutableStateOf(0)
+    var age by mutableStateOf("20")
         private set
 
    private val _uiEvent = Channel<UIEvent>()
@@ -29,11 +31,22 @@ class AgeViewModel @Inject constructor(private val prefs: Preferences) :
 
         viewModelScope.launch {
 
-            prefs.saveAge(age.value)
+            prefs.saveAge(age.toInt())
 
             _uiEvent.send(UIEvent.Navigate(route = Route.ACTIVITY))
         }
     }
 
-    fun onA
+    fun onAgeEnter(newAgeValue:String){
+
+
+      //keep age at 3 chars max
+        if (newAgeValue.length <=3){
+            /*filtering would be a business logic and is not allowed in
+            * ViewModel in strict clean Architecture.
+            *
+            * For Business logic we need to create Use Cases*/
+            age = newAgeValue.filter { a -> a.isDigit() }
+        }
+    }
 }
