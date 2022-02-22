ed84813528679e0f5b12ac9a179858ba14bafbaf
Antony <vontonnie@gmail.com>
2022-02-22 19:08:01 +0300

    Add HeightScreen TextField

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/height/HeightScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/height/HeightScreen.kt
@@ -1,22 +1,22 @@
 package com.uxstate.onboarding_presentation.height
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.uxstate.core_ui.LocalSpacing
 import com.uxstate.onboarding_presentation.R
 
 @Composable
 fun HeightScreen(viewModel: HeightViewModel = hiltViewModel(), scaffoldState: ScaffoldState) {
 
-
+val spacing = LocalSpacing.current
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
 
 
@@ -25,6 +25,10 @@ fun HeightScreen(viewModel: HeightViewModel = hiltViewModel(), scaffoldState: Sc
                 text = stringResource(id = R.string.whats_your_height),
                 style = MaterialTheme.typography.h3
             )
+
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+
+            TextField(value = viewModel.height, onValueChange = viewModel::onEnterHeight)
         }
     }
 }
