34516ab042d0147aa165e086e5e92fdfcd3a852b
Antony <vontonnie@gmail.com>
2022-02-22 20:10:36 +0300

    Add states to WeightViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/weight/WeightViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/weight/WeightViewModel.kt
@@ -1,9 +1,15 @@
 package com.uxstate.onboarding_presentation.weight
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.uxstate.core.domain.preferences.Preferences
 import com.uxstate.core.domain.use_cases.FilterOutDigits
+import com.uxstate.core.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -13,5 +19,17 @@ class WeightViewModel @Inject constructor(
 ) :
     ViewModel() {
 
+    var weight by mutableStateOf("80.0")
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEnterWeight(text: String) {
+
+        if (text.length <= 4) {
+
+
+        }
+    }
 
 }
