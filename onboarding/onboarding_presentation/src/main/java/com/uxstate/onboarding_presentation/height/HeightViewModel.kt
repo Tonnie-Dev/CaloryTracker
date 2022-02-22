29cb16980f3635423ef8c46bbc2a9b21af7c0fb2
Antony <vontonnie@gmail.com>
2022-02-22 18:46:00 +0300

    Add onEnterHeight() to HeightViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/height/HeightViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/height/HeightViewModel.kt
@@ -4,21 +4,48 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.core.domain.preferences.Preferences
 import com.uxstate.core.domain.use_cases.FilterOutDigits
 import com.uxstate.core.util.UIEvent
+import com.uxstate.core.util.UiText
+import com.uxstate.onboarding_presentation.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HeightViewModel @Inject constructor(private val prefs:Preferences, useCase:FilterOutDigits) :
+class HeightViewModel @Inject constructor(
+    private val prefs: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) :
     ViewModel() {
 
 
-        var height by mutableStateOf("180")
+    var height by mutableStateOf("180")
     private val _uiEvent = Channel<UIEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEnterHeight(text: String) {
+
+        viewModelScope.launch {
+
+
+            val heightNumber = text.toIntOrNull() ?: kotlin.run {
+
+                _uiEvent.send(UIEvent.ShowSnackbar(UiText.StringResource(R.string.whats_your_height)))
+
+                return@launch
+            }
+
+            height = filterOutDigits(heightNumber.toString())
+        }
+
+
+    }
+
+    
 
 }
