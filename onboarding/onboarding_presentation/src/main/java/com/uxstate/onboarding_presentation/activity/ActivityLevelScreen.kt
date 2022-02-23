42c36ae093dbe641ab9463b18b92e329a7e5b85a
Antony <vontonnie@gmail.com>
2022-02-23 08:41:51 +0300

    Add ActionButtons to ActivityLevelScreen

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelScreen.kt
@@ -1,21 +1,22 @@
 package com.uxstate.onboarding_presentation.activity
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.uxstate.core.domain.model.ActivityLevel
 import com.uxstate.core_ui.LocalSpacing
+import com.uxstate.onboarding_presentation.ActivityLevelViewModel
 import com.uxstate.onboarding_presentation.R
+import com.uxstate.onboarding_presentation.components.ActionButton
 
 
 @Composable
-fun ActivityLevelScreen() {
+fun ActivityLevelScreen(viewModel: ActivityLevelViewModel = hiltViewModel()) {
     val spacing = LocalSpacing.current
     Box(
         modifier = Modifier
@@ -29,6 +30,21 @@ fun ActivityLevelScreen() {
                 text = stringResource(id = R.string.whats_your_activity_level),
                 style = MaterialTheme.typography.h3
             )
+
+            Row(horizontalArrangement = Arrangement.SpaceBetween) {
+
+                ActionButton(text = stringResource(id = R.string.low), onclick = {
+
+                    viewModel.onSelectActivityLevel(ActivityLevel.Low)
+                })
+                ActionButton(text = stringResource(id = R.string.medium), onclick = {
+                    viewModel.onSelectActivityLevel(ActivityLevel.Medium)
+
+                })
+                ActionButton(text = stringResource(id = R.string.high), onclick = {
+                    viewModel.onSelectActivityLevel(ActivityLevel.High)
+                })
+            }
         }
     }
 
