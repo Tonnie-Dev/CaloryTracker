5b1a9e8c048789fc9afe3688db5845bf13bf9c6f
Antony <vontonnie@gmail.com>
2022-02-23 08:33:08 +0300

    Add onNextClick to the ActivityLevelViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelViewModel.kt
@@ -4,12 +4,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uxstate.core.domain.model.ActivityLevel
 import com.uxstate.core.domain.preferences.Preferences
+import com.uxstate.core.navigation.Route
 import com.uxstate.core.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -18,7 +21,7 @@ class ActivityLevelViewModel @Inject constructor(private val prefs: Preferences)
 
 
     var activityLevel by mutableStateOf<ActivityLevel>(ActivityLevel.Medium)
-        
+        private set
     private val _uiEvent = Channel<UIEvent>()
     val uiEvent = _uiEvent.receiveAsFlow()
 
@@ -29,5 +32,22 @@ class ActivityLevelViewModel @Inject constructor(private val prefs: Preferences)
         activityLevel = selectedActivityLevel
 
     }
+
+
+        fun onNextClick(){
+
+                viewModelScope.launch {
+
+                        //save level
+
+                        prefs.saveActivityLevel(activityLevel)
+
+                        //navigate
+                        _uiEvent.send(UIEvent.Navigate(route = Route.GOAL))
+
+                }
+
+
+        }
 
 }
