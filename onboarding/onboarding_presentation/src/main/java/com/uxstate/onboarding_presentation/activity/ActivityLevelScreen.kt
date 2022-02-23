483d16e9f2878d373c45b1d270316ff5afb1d472
Antony <vontonnie@gmail.com>
2022-02-23 08:50:44 +0300

    Change ActionButtons to SelectableButtons

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/activity/ActivityLevelScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.uxstate.core.domain.model.ActivityLevel
@@ -13,6 +14,7 @@ import com.uxstate.core_ui.LocalSpacing
 import com.uxstate.onboarding_presentation.ActivityLevelViewModel
 import com.uxstate.onboarding_presentation.R
 import com.uxstate.onboarding_presentation.components.ActionButton
+import com.uxstate.onboarding_presentation.components.SelectableButton
 
 
 @Composable
@@ -33,19 +35,36 @@ fun ActivityLevelScreen(viewModel: ActivityLevelViewModel = hiltViewModel()) {
 
             Row(horizontalArrangement = Arrangement.SpaceBetween) {
 
-                ActionButton(text = stringResource(id = R.string.low), onclick = {
+                SelectableButton(
+                    text = stringResource(id = R.string.low),
+                    isSelected = viewModel.activityLevel is ActivityLevel.Low,
+                    color =MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onclick = {
 
-                    viewModel.onSelectActivityLevel(ActivityLevel.Low)
-                })
-                ActionButton(text = stringResource(id = R.string.medium), onclick = {
-                    viewModel.onSelectActivityLevel(ActivityLevel.Medium)
+                        viewModel.onSelectActivityLevel(ActivityLevel.Low)
+                    })
 
-                })
-                ActionButton(text = stringResource(id = R.string.high), onclick = {
-                    viewModel.onSelectActivityLevel(ActivityLevel.High)
-                })
+                SelectableButton(
+                    text = stringResource(id = R.string.medium),
+                    isSelected = viewModel.activityLevel is ActivityLevel.Medium,
+                    color =MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onclick = { viewModel.onSelectActivityLevel(ActivityLevel.Medium) })
+
+                SelectableButton(
+                    text = stringResource(id = R.string.high),
+                    isSelected = viewModel.activityLevel is ActivityLevel.High,
+                    color =MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onclick = { viewModel.onSelectActivityLevel(ActivityLevel.High)})
+
             }
         }
+
+        ActionButton(text = stringResource(id = R.string.next), onclick = viewModel::onNextClick)
+
+
     }
 
 }
