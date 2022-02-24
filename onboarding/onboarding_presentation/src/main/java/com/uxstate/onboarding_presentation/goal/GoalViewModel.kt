4c9617e26d65a7f7bdb813552fd53c9048e5bf65
Antony <vontonnie@gmail.com>
2022-02-24 07:01:13 +0300

    Add GoalViewModel states

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/goal/GoalViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/goal/GoalViewModel.kt
@@ -1,11 +1,24 @@
 package com.uxstate.onboarding_presentation.goal
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.uxstate.core.domain.model.GoalType
+import com.uxstate.core.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import java.util.prefs.Preferences
 import javax.inject.Inject
 
 @HiltViewModel
 class GoalViewModel @Inject constructor(private val prefs: Preferences) :
-        ViewModel() {
+    ViewModel() {
+
+    var goal by mutableStateOf<GoalType>(GoalType.KeepWeight)
+        private set
+
+    private val _uiState = Channel<UIEvent>()
+
+
 }
