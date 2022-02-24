f4bb903113aade782aca95c37605137b1676f47f
Antony <vontonnie@gmail.com>
2022-02-24 07:25:53 +0300

    Add Selected Buttons row to GoalScreen composable

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/goal/GoalScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/goal/GoalScreen.kt
@@ -1,14 +1,19 @@
 package com.uxstate.onboarding_presentation.goal
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.uxstate.core.domain.model.GoalType
 import com.uxstate.core.util.UIEvent
 import com.uxstate.core_ui.LocalSpacing
+import com.uxstate.onboarding_presentation.R
+import com.uxstate.onboarding_presentation.components.SelectableButton
 
 @Composable
 fun GoalScreen(
@@ -24,8 +29,47 @@ fun GoalScreen(
                 .padding(spacing.spaceLarge), contentAlignment = Alignment.Center
     ) {
 
+        Column() {
 
-        
+            Text(
+                text = stringResource(id = R.string.lose_keep_or_gain_weight),
+                style = MaterialTheme.typography.h3
+            )
+
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+
+                SelectableButton(
+                    text = stringResource(id = R.string.lose),
+                    isSelected = viewModel.goal is GoalType.LoseWeight,
+                    color = Color.White,
+                    selectedTextColor = MaterialTheme.colors.primaryVariant,
+                    onclick = {
+
+                        viewModel.onSelectGoalType(GoalType.LoseWeight)
+                    })
+                
+                SelectableButton(
+                    text = stringResource(id = R.string.keep),
+                    isSelected = viewModel.goal is GoalType.KeepWeight,
+                    color = Color.White ,
+                    selectedTextColor = MaterialTheme.colors.primaryVariant,
+                    onclick = { viewModel.onSelectGoalType(GoalType.KeepWeight)})
+
+                SelectableButton(
+                    text = stringResource(id = R.string.gain) ,
+                    isSelected =viewModel.goal is GoalType.GainWeight ,
+                    color = Color.White ,
+                    selectedTextColor = MaterialTheme.colors.primaryVariant,
+                    onclick = { viewModel.onSelectGoalType(GoalType.GainWeight)})
+
+            }
+        }
+
+
     }
 
 }
