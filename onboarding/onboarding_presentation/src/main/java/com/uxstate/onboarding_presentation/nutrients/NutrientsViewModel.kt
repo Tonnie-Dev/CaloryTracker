e2d02124eb33f53ddccdeba922304f4bcc5b24dd
Antony <vontonnie@gmail.com>
2022-02-26 15:20:56 +0300

    Add onEnterFats() in the NutrientsViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/nutrients/NutrientsViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/nutrients/NutrientsViewModel.kt
@@ -2,17 +2,50 @@ package com.uxstate.onboarding_presentation.nutrients
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.uxstate.core.domain.preferences.Preferences
+import com.uxstate.core.domain.use_cases.FilterOutDigits
+import com.uxstate.core.util.UIEvent
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class NutrientsViewModel @Inject constructor(private val prefs:Preferences) :
-        ViewModel() {
+class NutrientsViewModel @Inject constructor(
+    private val prefs: Preferences,
+    private val filterOutDigits: FilterOutDigits
+) :
+    ViewModel() {
 
 
-            val carbs by mutableStateOf("40")
-            val proteins by mutableStateOf("30")
-            val fats by mutableStateOf("30")
+    var carbs by mutableStateOf("40")
+        private set
+
+    var proteins by mutableStateOf("30")
+        private set
+
+    var fats by mutableStateOf("30")
+        private set
+
+
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEnterCarbs(carbsValue: String) {
+
+        carbs = filterOutDigits(carbsValue)
+    }
+
+
+    fun onEnterProteins(proteinsValue: String) {
+
+        proteins = filterOutDigits(proteinsValue)
+    }
+
+    fun onEnterFats(fatsValue: String) {
+
+        fats = fatsValue
+    }
 }
