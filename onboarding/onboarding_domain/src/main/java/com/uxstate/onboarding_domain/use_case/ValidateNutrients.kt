f5ab679dc21cbcd20e3462db47d4ccc7abe53e4c
Antony <vontonnie@gmail.com>
2022-02-27 15:01:52 +0300

    Add business logic for the invoke() for ValidateNutrients usecase

--- onboarding/onboarding_domain/src/main/java/com/uxstate/onboarding_domain/use_case/ValidateNutrients.kt
+++ onboarding/onboarding_domain/src/main/java/com/uxstate/onboarding_domain/use_case/ValidateNutrients.kt
@@ -1,6 +1,7 @@
 package com.uxstate.onboarding_domain.use_case
 
 import com.uxstate.core.util.UiText
+import com.uxstate.onboarding_domain.R
 
 
 //validates the TextFields values and ensures they add up to 100
@@ -9,10 +10,39 @@ class ValidateNutrients {
     //invoke takes 3 parameters needed for its logic & returns a Result Object
     operator fun invoke(
         carbsRatioText: String,
-        proteinsTextRatio: String,
+        proteinsRatioText: String,
         fatsRatioText: String
-    ):Result {
+    ): Result {
 
+        //parse parameters to ints
+
+        val carbsRatio = carbsRatioText.toIntOrNull()
+        val proteinsRatio = proteinsRatioText.toIntOrNull()
+        val fatsRatio = fatsRatioText.toIntOrNull()
+
+        //check for nulls
+
+        if (carbsRatio == null || proteinsRatio == null || fatsRatio == null) {
+
+            //respond to the ViewModel with an error
+            return Result.Error(message = UiText.StringResource(R.string.error_invalid_values))
+        }
+
+        //check for 100% toal
+        else if ((carbsRatio + proteinsRatio + fatsRatio) != 100) {
+
+            //respond to the ViewModel with an error
+            return Result.Error(message = UiText.StringResource(R.string.error_not_100_percent))
+
+            //else success
+        } else {
+
+            return Result.Success(
+                carbsRatio = carbsRatio,
+                proteinsRatio = proteinsRatio,
+                fatsRatio = fatsRatio
+            )
+        }
 
     }
 
