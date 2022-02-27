3b7d485b915b49dceb3653e0fbcfdaddb48d2495
Antony <vontonnie@gmail.com>
2022-02-27 14:27:02 +0300

    Add States to the NutrientsViewModel

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/nutrients_goal/NutrientsViewModel.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/nutrients_goal/NutrientsViewModel.kt
@@ -4,17 +4,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.uxstate.core.domain.preferences.Preferences
 import com.uxstate.core.domain.use_cases.FilterOutDigits
-import com.uxstate.core.navigation.Route
 import com.uxstate.core.util.UIEvent
-import com.uxstate.core.util.UiText
-import com.uxstate.onboarding_presentation.R
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -22,4 +17,13 @@ class NutrientsViewModel @Inject constructor(
     private val prefs: Preferences,
     private val filterOutDigits: FilterOutDigits
 ) :
-    ViewModel() {}
+    ViewModel() {
+
+    //state
+    var state by mutableStateOf(NutrientsGoalState())
+        private set
+
+    //channel
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+}
