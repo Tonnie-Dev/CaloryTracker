7b74e9b1dec81fa811bddc0bab80ef4f7cc6ccdc
Antony <vontonnie@gmail.com>
2022-02-27 14:47:31 +0300

    Add  Result sealed class inside ValidateNutrients

--- onboarding/onboarding_domain/src/main/java/com/uxstate/onboarding_domain/use_case/ValidateNutrients.kt
+++ onboarding/onboarding_domain/src/main/java/com/uxstate/onboarding_domain/use_case/ValidateNutrients.kt
@@ -1,15 +1,17 @@
 package com.uxstate.onboarding_domain.use_case
 
+import com.uxstate.core.util.UiText
+
 
 //validates the TextFields values and ensures they add up to 100
 class ValidateNutrients {
 
-    //invoke takes 3 parameters needed for its logic
+    //invoke takes 3 parameters needed for its logic & returns a Result Object
     operator fun invoke(
         carbsRatioText: String,
         proteinsTextRatio: String,
         fatsRatioText: String
-    ) {
+    ):Result {
 
 
     }
@@ -17,5 +19,14 @@ class ValidateNutrients {
     //class representing the validation result to notify ViewModel
     //directly from the use case
 
-    sealed class Result {}
+    sealed class Result {
+
+        data class Success(val carbsRatio: Int, val proteinsRatio: Int, val fatsRatio: Int) :
+            Result() {
+
+
+        }
+
+        data class Error(val message: UiText) : Result()
+    }
 }
