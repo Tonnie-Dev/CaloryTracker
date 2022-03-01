17906f2a450729d2456b8529a3def8263e2b9d5c
Antony <vontonnie@gmail.com>
2022-03-01 08:24:27 +0300

    Add TrackerFoodEntity class

---
+++ tracker/tracker_data/src/main/java/com/uxstate/tracker_data/local/entity/TrackerFoodEntity.kt
@@ -0,0 +1,22 @@
+package com.uxstate.tracker_data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity()
+data class TrackerFoodEntity(
+
+    //room autogenerates this for us
+    @PrimaryKey val id: Int? = null,
+    val name: String,
+    val carbs: Int,
+    val protein: Int,
+    val fats: Int,
+    val imageUrl: String?,
+    val type: String,
+    val amount: Int,
+    val dayOfMonth: Int,
+    val month: Int,
+    val year: Int,
+    val calories: Int
+)
