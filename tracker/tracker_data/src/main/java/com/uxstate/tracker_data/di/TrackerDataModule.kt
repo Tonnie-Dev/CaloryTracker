6b449fc6af99ada5bededd8b9bf823dcebaf9dd4
Antony <vontonnie@gmail.com>
2022-03-01 08:07:46 +0300

    Add provideOkHttpClient() to the TrakcerDataModule

--- tracker/tracker_data/src/main/java/com/uxstate/tracker_data/di/TrackerDataModule.kt
+++ tracker/tracker_data/src/main/java/com/uxstate/tracker_data/di/TrackerDataModule.kt
@@ -1,12 +1,12 @@
 package com.uxstate.tracker_data.di
 
-import com.squareup.moshi.Moshi
 import com.uxstate.tracker_data.remote.OpenFoodAPI
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
@@ -15,14 +15,25 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 object TrackerDataModule {
 
+    /*Add interceptor to log our network requests
+    *
+    * This is helpful in debugging*/
+
     @Provides
     @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
 
-    fun provideOpenFoodAPI(client:OkHttpClient) :OpenFoodAPI{
+        return OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor().apply {
+                    level = HttpLoggingInterceptor.Level.BODY
+                }).build()
+    }
 
-
+    @Provides
+    @Singleton
+    fun provideOpenFoodAPI(client: OkHttpClient): OpenFoodAPI {
         return Retrofit.Builder()
-                .baseUrl("")
+                .baseUrl(OpenFoodAPI.BASE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .client(client)
                 .build()
