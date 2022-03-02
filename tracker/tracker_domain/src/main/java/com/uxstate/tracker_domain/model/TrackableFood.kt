43cc2034d649a939ea2f887bf37f03edc949ea26
Antony <vontonnie@gmail.com>
2022-03-02 08:18:27 +0300

    Add TrackableFood model class

---
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/model/TrackableFood.kt
@@ -0,0 +1,10 @@
+package com.uxstate.tracker_domain.model
+
+data class TrackableFood(
+    val name: String,
+    val carbsPer100g: Int,
+    val proteinPer100g: Int,
+    val fatsPer100g: Int,
+    val imageUrl: String?,
+    val caloriesPer100g: Int
+)
