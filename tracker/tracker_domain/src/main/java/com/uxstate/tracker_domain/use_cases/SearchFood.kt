3adbf2be692596b1061f2d0b018e7297caa28a2b
Antony <vontonnie@gmail.com>
2022-03-04 08:05:14 +0300

    Add SearchFood use case class

---
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/SearchFood.kt
@@ -0,0 +1,8 @@
+package com.uxstate.tracker_domain.use_cases
+
+import com.uxstate.tracker_domain.repository.TrackerRepository
+
+class SearchFood(private val repository: TrackerRepository) {
+
+    
+}
