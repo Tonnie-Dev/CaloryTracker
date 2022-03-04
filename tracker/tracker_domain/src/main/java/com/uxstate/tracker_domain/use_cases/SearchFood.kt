e59371c11b54d1902638f96410e8909f653e432b
Antony <vontonnie@gmail.com>
2022-03-04 08:36:01 +0300

    Add invoke operator fxn to SearchFood use case

--- tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/SearchFood.kt
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/SearchFood.kt
@@ -1,8 +1,30 @@
 package com.uxstate.tracker_domain.use_cases
 
+import com.uxstate.tracker_domain.model.TrackableFood
 import com.uxstate.tracker_domain.repository.TrackerRepository
 
+//use-case should be accessing UI or Data(DTOs, DB etc)
+
+//The repository passed in is the definitions inside the domain layer
 class SearchFood(private val repository: TrackerRepository) {
 
-    
+    suspend operator fun invoke(
+        query: String,
+        page: Int = 1,
+        pageSize: Int = 40
+    ): Result<List<TrackableFood>> {
+
+
+        //check if the query is blank and return an empty list
+        if (query.isBlank()) {
+
+            return Result.success(listOf())
+        }
+
+        //trim() eliminates leading and trailing spaces
+        return repository.searchFood(query = query.trim(), page = page, pageSize = pageSize)
+
+
+    }
+
 }
