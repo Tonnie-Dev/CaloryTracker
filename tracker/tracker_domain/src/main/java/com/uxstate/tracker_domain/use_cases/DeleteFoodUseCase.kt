dbdbb25cf8670d82ec094e1e7736923549a5fab0
Antony <vontonnie@gmail.com>
2022-03-04 20:26:25 +0300

    Add DeleteFoodUseCase class

---
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/DeleteFoodUseCase.kt
@@ -0,0 +1,6 @@
+package com.uxstate.tracker_domain.use_cases
+
+import com.uxstate.tracker_domain.repository.TrackerRepository
+
+class DeleteFoodUseCase(private val repository: TrackerRepository) {
+}
