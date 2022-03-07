5ccb3ebcf4b367fd3fcbbc8ae16cdfc968593db7
Antony <vontonnie@gmail.com>
2022-03-07 07:10:22 +0300

    Add CalculateMealNutrientsUseCase

---
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/CalculateMealNutrientsUseCase.kt
@@ -0,0 +1,7 @@
+package com.uxstate.tracker_domain.use_cases
+
+import com.uxstate.core.domain.preferences.Preferences
+
+/*use SharedPreferences to retrieve values entered during onboarding*/
+class CalculateMealNutrientsUseCase(private val prefs:Preferences) {
+}
