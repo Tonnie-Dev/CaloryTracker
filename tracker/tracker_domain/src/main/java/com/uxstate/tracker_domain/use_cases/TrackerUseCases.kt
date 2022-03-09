bb25fbe2da38112c8d707d939930364bc8a0b68b
Antony <vontonnie@gmail.com>
2022-03-09 19:44:50 +0300

    Load use-cases onto TrackerUseCases container

--- tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/TrackerUseCases.kt
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/use_cases/TrackerUseCases.kt
@@ -1,3 +1,11 @@
 package com.uxstate.tracker_domain.use_cases
 
-data class TrackerUseCases()
+
+//helps make the viewModel not have many constructor injections
+data class TrackerUseCases(
+    val calculateMealNutrientsUseCase: CalculateMealNutrientsUseCase,
+    val deleteFoodUseCase: DeleteFoodUseCase,
+    val getFoodsForDateUseCase: GetFoodsForDateUseCase,
+    val searchFoodUseCase: SearchFoodUseCase,
+    val trackFoodUseCase: TrackFoodUseCase
+)
