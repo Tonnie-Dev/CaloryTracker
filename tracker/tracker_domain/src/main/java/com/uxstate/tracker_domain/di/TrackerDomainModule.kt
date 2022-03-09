21edaae2d498b96930e8649a5e3831cd3ca2c255
Antony <vontonnie@gmail.com>
2022-03-09 19:54:40 +0300

    Scope TrackerDomainModule to the ViewModel scope

--- tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/di/TrackerDomainModule.kt
+++ tracker/tracker_domain/src/main/java/com/uxstate/tracker_domain/di/TrackerDomainModule.kt
@@ -4,16 +4,20 @@ import com.uxstate.tracker_domain.use_cases.*
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+
+//use cases should be scoped to ViewModel scope as they are used by the ViewModels
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 
 object TrackerDomainModule {
 
     @Provides
-    @Singleton
+    @ViewModelScoped
 
     fun provideTrackerUseCases(
         calculateMealNutrientsUseCase: CalculateMealNutrientsUseCase,
