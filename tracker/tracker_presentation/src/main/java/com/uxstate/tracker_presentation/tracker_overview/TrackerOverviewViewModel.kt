ad461e8a2705629bdb37aa85a915b1bdabb50398
Antony <vontonnie@gmail.com>
2022-03-10 08:31:57 +0300

    Define TrackerOverviewViewModel class

---
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/TrackerOverviewViewModel.kt
@@ -0,0 +1,12 @@
+package com.uxstate.tracker_presentation.tracker_overview
+
+import androidx.lifecycle.ViewModel
+import com.uxstate.tracker_domain.use_cases.TrackerUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class TrackerOverviewViewModel @Inject constructor(private val trackerUseCases:TrackerUseCases) :
+        ViewModel() {
+}
