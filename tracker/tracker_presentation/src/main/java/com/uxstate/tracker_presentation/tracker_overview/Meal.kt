78573741ced8fbd360ce9216aa9a4624fc92db87
Antony <vontonnie@gmail.com>
2022-03-10 07:44:16 +0300

    Add class Meal for UI State

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/Meal.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/Meal.kt
@@ -1,5 +1,21 @@
 package com.uxstate.tracker_presentation.tracker_overview
 
-class Meal {
+import androidx.annotation.DrawableRes
+import com.uxstate.tracker_domain.model.MealType
+
+/*the class is declared in the presentation layoyer and not in the domail layer
+ This is because here we are dealing with UI state
+
+ This is class is only used to reflect state for the UI and is not needed in
+ the data or the domain layers*/
+data class Meal(
+    val name: String,
+    @DrawableRes val drawableRes: Int,
+    val mealType: MealType,
+    val carbs: Int,
+    val proteins: Int,
+    val fates: Int,
+    val isExpanded: Boolean
+) {
 
 }
