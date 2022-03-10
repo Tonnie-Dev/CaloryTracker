3aff2d2b3258a5274fbe32617cdccd8f995aedb2
Antony <vontonnie@gmail.com>
2022-03-10 08:04:02 +0300

    Add default meals list

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/Meal.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/Meal.kt
@@ -3,6 +3,7 @@ package com.uxstate.tracker_presentation.tracker_overview
 import androidx.annotation.DrawableRes
 import com.uxstate.core.util.UiText
 import com.uxstate.tracker_domain.model.MealType
+import com.uxstate.tracker_presentation.R
 
 /*the class is declared in the presentation layoyer and not in the domail layer
  This is because here we are dealing with UI state
@@ -13,10 +14,35 @@ data class Meal(
     val name: UiText,
     @DrawableRes val drawableRes: Int,
     val mealType: MealType,
-    val carbs: Int,
-    val proteins: Int,
-    val fates: Int,
-    val isExpanded: Boolean
-) {
+    val carbs: Int = 0,
+    val proteins: Int = 0,
+    val fates: Int = 0,
+    val isExpanded: Boolean = false
+)
 
-}
+val defaultMeals = listOf(
+    Meal(
+        name = UiText.StringResource(R.string.breakfast),
+        drawableRes = R.drawable.ic_breakfast,
+        mealType = MealType.Breakfast,
+
+        ),
+    Meal(
+        name = UiText.StringResource(R.string.lunch),
+        drawableRes = R.drawable.ic_lunch,
+        mealType = MealType.Lunch,
+
+        ),
+    Meal(
+        name = UiText.StringResource(R.string.dinner),
+        drawableRes = R.drawable.ic_dinner,
+        mealType = MealType.Dinner,
+
+        ),
+    Meal(
+        name = UiText.StringResource(R.string.snacks),
+        drawableRes = R.drawable.ic_snack,
+        mealType = MealType.Snack,
+
+        ),
+)
