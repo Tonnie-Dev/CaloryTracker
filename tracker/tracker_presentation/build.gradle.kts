88cf262040f7e25270d25ed4a9a80d939a74e646
Antony <vontonnie@gmail.com>
2022-03-13 08:29:13 +0300

    Add debug tooling in the compose-module.gradle file

--- tracker/tracker_presentation/build.gradle.kts
+++ tracker/tracker_presentation/build.gradle.kts
@@ -7,6 +7,5 @@ dependencies{
     "implementation"(project(Modules.core))
     "implementation"(project(Modules.coreUi))
     "implementation"(project(Modules.trackerDomain))
-
     "implementation"(Coil.coilCompose)
 }
