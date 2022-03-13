b287abaf0ad18b4f4cd517f7bf15f6d3aa58782d
Antony <vontonnie@gmail.com>
2022-03-13 09:44:10 +0300

    Add NutrientsBar

---
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBar.kt
@@ -0,0 +1,16 @@
+package com.uxstate.tracker_presentation.tracker_overview.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NutrientsBar(
+    carbs: Int,
+    proteins: Int,
+    fats: Int,
+    calories: Int,
+    caloriesGoal: Int,
+    modifier: Modifier = Modifier
+) {
+
+}
