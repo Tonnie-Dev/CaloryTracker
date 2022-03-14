48c717a478ba87207213a477d80128650d186642
Antony <vontonnie@gmail.com>
2022-03-14 08:19:58 +0300

    Add LaunchedEffect Block to NutrientsBarInfo composable

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBarInfo.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBarInfo.kt
@@ -1,6 +1,11 @@
 package com.uxstate.tracker_presentation.tracker_overview.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
@@ -16,4 +21,19 @@ fun NutrientsBarInfo(
     strokeWidth: Dp = 8.dp
 ) {
 
+
+    val background = MaterialTheme.colors.background
+    val goalExceededColor = MaterialTheme.colors.error
+    val angelRatio = remember { Animatable(0f) }
+
+    //use value to animate bar when value changes
+    LaunchedEffect(key1 = value, block = {
+
+        //animate
+        angelRatio.animateTo(
+            targetValue = if (goal > 0) value / goal.toFloat() else 0f,
+            animationSpec = tween(durationMillis = 300)
+        )
+
+    })
 }
