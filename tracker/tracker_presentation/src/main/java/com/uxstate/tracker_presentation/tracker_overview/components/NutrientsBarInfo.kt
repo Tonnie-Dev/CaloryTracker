a17c902664f742c66b2d6d6031cc077b5de37bb4
Antony <vontonnie@gmail.com>
2022-03-14 08:01:40 +0300

    Add NutrientsBarInfo composable

---
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBarInfo.kt
@@ -0,0 +1,19 @@
+package com.uxstate.tracker_presentation.tracker_overview.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun NutrientsBarInfo(
+    value: Int,
+    goal: Int,
+    name: String,
+    color: Color,
+    modifier: Modifier = Modifier,
+    strokeWidth: Dp = 8.dp
+) {
+
+}
