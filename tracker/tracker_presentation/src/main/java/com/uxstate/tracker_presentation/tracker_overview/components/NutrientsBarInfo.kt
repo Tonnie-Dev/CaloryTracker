b9e73def04a1728d5de33fe2c7f4793a6c526d8e
Antony <vontonnie@gmail.com>
2022-03-15 08:39:38 +0300

    Draw the inner text

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBarInfo.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/NutrientsBarInfo.kt
@@ -4,9 +4,11 @@ import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -15,8 +17,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.uxstate.tracker_presentation.R
+import com.uxstate.tracker_presentation.components.UnitDisplay
 
 @Composable
 fun NutrientsBarInfo(
@@ -85,5 +91,23 @@ fun NutrientsBarInfo(
 
         )
 
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            UnitDisplay(
+                amount = value,
+                unit = stringResource(id = R.string.grams),
+                amountColor = if (value <= goal) MaterialTheme.colors.onPrimary else goalExceededColor,
+                unitColor = if (value <= goal) MaterialTheme.colors.onPrimary else goalExceededColor
+            )
+            Text(
+                text = name,
+                fontWeight = FontWeight.Light,
+                color = if (value <= goal) MaterialTheme.colors.onPrimary else goalExceededColor,
+                style = MaterialTheme.typography.body1
+            )
+        }
     }
 }
