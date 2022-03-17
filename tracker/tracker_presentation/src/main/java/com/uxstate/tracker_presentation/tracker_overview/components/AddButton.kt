c2491c11e52cee62786565fde07af7680ea234a6
Antony <vontonnie@gmail.com>
2022-03-17 07:39:14 +0300

    Add components for AddButton Composable

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/AddButton.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/tracker_overview/components/AddButton.kt
@@ -1,9 +1,24 @@
 package com.uxstate.tracker_presentation.tracker_overview.components
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.uxstate.core_ui.LocalSpacing
+import com.uxstate.tracker_presentation.R
 
 
 @Composable
@@ -13,5 +28,28 @@ fun AddButton(
     modifier: Modifier = Modifier,
     color: Color = MaterialTheme.colors.primary
 ) {
+    val spacing = LocalSpacing.current
 
+    Row(
+        modifier = modifier
+                .clip(RoundedCornerShape(100.dp))
+                .clickable { onClick() }
+                .border(width = 1.dp, color = color, shape = RoundedCornerShape(100.dp))
+                .padding(spacing.spaceMedium),
+        
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+
+        Icon(imageVector = Icons.Default.Add, contentDescription = stringResource(id = R.string.add), tint = color)
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        Text(text = text, style = MaterialTheme.typography.button, color = color)
+        
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAddButton() {
+    AddButton(text = "Add", onClick = { /*TODO*/ })
 }
