941fecd60d740c7892e0e1319665aa00a8a7e8b1
Antony <vontonnie@gmail.com>
2022-03-20 07:35:15 +0300

    Add SearchViewModel

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchViewModel.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchViewModel.kt
@@ -1,10 +1,14 @@
 package com.uxstate.tracker_presentation.search
 
 import androidx.lifecycle.ViewModel
+import com.uxstate.core.domain.use_cases.FilterOutDigits
+import com.uxstate.tracker_domain.use_cases.TrackerUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SearchViewModel @Inject constructor(private val ) :
-        ViewModel() {
+class SearchViewModel @Inject constructor(
+    private val trackerUseCases: TrackerUseCases,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
 }
