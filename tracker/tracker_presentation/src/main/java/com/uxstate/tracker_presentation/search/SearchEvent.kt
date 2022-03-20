cc7f89f2ad554ea1ed3292b65d697336992b1fb0
Antony <vontonnie@gmail.com>
2022-03-20 07:01:21 +0300

    Add SearchEvent Class

---
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchEvent.kt
@@ -0,0 +1,17 @@
+package com.uxstate.tracker_presentation.search
+
+import com.uxstate.tracker_domain.model.MealType
+import com.uxstate.tracker_domain.model.TrackableFood
+import java.time.LocalDate
+
+sealed class SearchEvent {
+
+
+    data class OnQueryChange(val query: String) : SearchEvent()
+    object OnSearch : SearchEvent()
+    data class OnToggleTrackableFood(val food: TrackableFood) : SearchEvent()
+    data class OnAmountForFoodChange(val food: TrackableFood, val amount: Int) : SearchEvent()
+    data class OnTrackFoodClick(val food: TrackableFood, val mealType: MealType, val date:LocalDate):SearchEvent()
+    //when we click on the search field to trigger hint hiding for basic TextField
+    data class OnSearchFocusChange(val isFocused:Boolean):SearchEvent()
+}
