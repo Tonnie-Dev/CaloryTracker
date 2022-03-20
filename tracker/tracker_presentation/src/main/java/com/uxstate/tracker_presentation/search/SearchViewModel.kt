917bfe23d7b357b3cc052b8044998f7a9f9d5e80
Antony <vontonnie@gmail.com>
2022-03-20 07:42:00 +0300

    Add Event channel to the ViewModel

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchViewModel.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchViewModel.kt
@@ -1,9 +1,15 @@
 package com.uxstate.tracker_presentation.search
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.uxstate.core.domain.use_cases.FilterOutDigits
+import com.uxstate.core.util.UIEvent
 import com.uxstate.tracker_domain.use_cases.TrackerUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
@@ -11,4 +17,15 @@ class SearchViewModel @Inject constructor(
     private val trackerUseCases: TrackerUseCases,
     private val filterOutDigits: FilterOutDigits
 ) : ViewModel() {
+
+
+    //state
+    var state by mutableStateOf(SearchState())
+    private set
+
+    //event
+    private val _uiEvent = Channel<UIEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    
 }
