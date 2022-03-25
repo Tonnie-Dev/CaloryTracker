e6af8a5b53214396b16e78e82fb681429e66fc45
Antony <vontonnie@gmail.com>
2022-03-25 08:31:14 +0300

    Add SearchTextField to SearchScreen composable

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/SearchScreen.kt
@@ -1,24 +1,30 @@
 package com.uxstate.tracker_presentation.search
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.uxstate.core.util.UIEvent
 import com.uxstate.core_ui.LocalSpacing
+import com.uxstate.tracker_presentation.R
+import com.uxstate.tracker_presentation.search.components.SearchTextField
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchScreen(
     viewModel: SearchViewModel = hiltViewModel(),
     scaffoldState: ScaffoldState,
+    mealName:String,
+    month: Int,
+    year:Int,
     onNavigateUp: () -> Unit
 ) {
 
@@ -66,6 +72,20 @@ fun SearchScreen(
                 .padding(spacing.spaceMedium)
     ) {
 
+        Text(text = stringResource(id = R.string.add_meal,mealName), style = MaterialTheme.typography.h2)
+
+        Spacer(modifier = Modifier.height(spacing.spaceMedium))
+
+        SearchTextField(
+            text = state.query,
+            hint = stringResource(id = R.string.search),
+            onValueChange = {
+                            viewModel.onEvent(SearchEvent.OnQueryChange(it))
+            } ,
+            onSearch = { viewModel.onEvent(SearchEvent.OnSearch)},
+            onFocusChange = {
+                viewModel.onEvent(SearchEvent.OnSearchFocusChange(it.isFocused))}
+        )
     }
 
 }
