35e99772720e95a329a964ffecd8983de36b3f81
Tonnie <vontonnie@gmail.com>
2022-03-29 20:57:02 +0300

    Introduce local variables to TrackableFoodItemComposable

--- tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/components/TrackableFoodItem.kt
+++ tracker/tracker_presentation/src/main/java/com/uxstate/tracker_presentation/search/components/TrackableFoodItem.kt
@@ -1,8 +1,20 @@
 package com.uxstate.tracker_presentation.search.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.uxstate.core_ui.LocalSpacing
+import com.uxstate.tracker_presentation.search.TrackableFoodUiState
+
 
 @Composable
-fun TrackableFoodItem() {
-    
+fun TrackableFoodItem(
+    trackableFoodUiState: TrackableFoodUiState,
+    onClick: () -> Unit,
+    onAmountChange: (String) -> Unit,
+    onTrack:() -> Unit, modifier: Modifier = Modifier
+){
+
+    val food = trackableFoodUiState.food
+    val spacing = LocalSpacing.current
+
 }
