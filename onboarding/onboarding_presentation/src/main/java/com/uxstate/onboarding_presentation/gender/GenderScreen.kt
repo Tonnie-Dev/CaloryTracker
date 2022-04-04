e9fd9ec67f5d43ced1820c3faf110e599104e5a9
Tonnie <vontonnie@gmail.com>
2022-04-04 08:13:48 +0300

    Refactor onNavigate to onNextClick on GenderScreen

--- onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/gender/GenderScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/uxstate/onboarding_presentation/gender/GenderScreen.kt
@@ -22,7 +22,7 @@ import kotlinx.coroutines.flow.collect
 @Composable
 fun GenderScreen(
     viewModel: GenderViewModel = hiltViewModel(),
-    onNavigate: (UIEvent.Navigate) -> Unit
+    onNextClick: () -> Unit
 ) {
 
     val spacing = LocalSpacing.current
@@ -40,7 +40,7 @@ fun GenderScreen(
 
                 is UIEvent.Navigate -> {
 
-                    onNavigate(event)
+                   onNextClick()
                 }
 
                 //ignore
