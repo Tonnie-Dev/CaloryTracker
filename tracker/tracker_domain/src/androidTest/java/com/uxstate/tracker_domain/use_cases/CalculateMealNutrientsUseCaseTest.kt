9368ec588db5f4bb0545900d098de6e9b471b347
Tonnie <vontonnie@gmail.com>
2022-04-15 18:40:52 +0300

    Create random TrackedFood Items

--- tracker/tracker_domain/src/androidTest/java/com/uxstate/tracker_domain/use_cases/CalculateMealNutrientsUseCaseTest.kt
+++ tracker/tracker_domain/src/androidTest/java/com/uxstate/tracker_domain/use_cases/CalculateMealNutrientsUseCaseTest.kt
@@ -5,12 +5,14 @@ import com.uxstate.core.domain.model.Gender
 import com.uxstate.core.domain.model.GoalType
 import com.uxstate.core.domain.model.UserInfo
 import com.uxstate.core.domain.preferences.Preferences
+import com.uxstate.tracker_domain.model.MealType
 import com.uxstate.tracker_domain.model.TrackedFood
 import io.mockk.every
 import io.mockk.mockk
 import org.junit.Before
 import org.junit.Test
-import org.junit.Assert.*
+import java.time.LocalDate
+import kotlin.random.Random
 
 class CalculateMealNutrientsUseCaseTest {
     
@@ -22,31 +24,55 @@ class CalculateMealNutrientsUseCaseTest {
         
         // use mockk<> () method to specify object to be mocked
         val prefs = mockk<Preferences>(relaxed = true)
-    
+        
         // use every to define what behaviour is going to be mocked
         every { prefs.loadUserInfo() } returns (UserInfo(
             gender = Gender.Male,
-            age = 28,weight = 80f,
+            age = 28,
+            weight = 80f,
             height = 180,
             activityLevel = ActivityLevel.Medium,
             goalType = GoalType.KeepWeight,
             carbRatio = 0.4f,
             proteinRatio = 0.4f,
-            fatRatio = 0.3f))
+            fatRatio = 0.3f
+        ))
         
         //we pass in the mocked prefs object instead of the real one
         calculateMealNutrientsUseCase = CalculateMealNutrientsUseCase(prefs)
     }
     
     @Test
-    fun caloriesForBreakfastProperlyCalculated(){
-    
-        //create some 30 trackedFood objectes
+    fun caloriesForBreakfastProperlyCalculated() {
+        
+        //create some 30 trackedFood objects
+        
+        val trackedFoods = (1..30).map {
+            
+            TrackedFood(
+                name = "name",
+                carbs = Random.nextInt(100),
+                protein = Random.nextInt(100),
+                fat = Random.nextInt(100),
+                imageUrl = "",
+                mealType = MealType.fromString(
+                    listOf(
+                        "breakfast",
+                        "lunch",
+                        "dinner",
+                        "snack"
+                    ).random()
+                ),
+                amount = 100,
+                date = LocalDate.now(),
+                calories = Random.nextInt(100),
+                id = null
+            )
+        }
         
         
-    
+        
     }
     
-   
     
 }
