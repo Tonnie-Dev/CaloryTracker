12badff505cbc21ddd3969c7296e54a52d25317c
Tonnie <vontonnie@gmail.com>
2022-04-15 13:53:21 +0300

    Use every block to stup.prefs.loadUserInfo

--- tracker/tracker_domain/src/androidTest/java/com/uxstate/tracker_domain/use_cases/CalculateMealNutrientsUseCaseTest.kt
+++ tracker/tracker_domain/src/androidTest/java/com/uxstate/tracker_domain/use_cases/CalculateMealNutrientsUseCaseTest.kt
@@ -1,4 +1,38 @@
 package com.uxstate.tracker_domain.use_cases
 
+import com.uxstate.core.domain.model.ActivityLevel
+import com.uxstate.core.domain.model.Gender
+import com.uxstate.core.domain.model.GoalType
+import com.uxstate.core.domain.model.UserInfo
+import com.uxstate.core.domain.preferences.Preferences
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Before
+
+
 class CalculateMealNutrientsUseCaseTest {
+    
+    private lateinit var calculateMealNutrientsUseCase: CalculateMealNutrientsUseCase
+    
+    
+    @Before
+    fun setUp() {
+        
+        // use mockk<> () method to specify object to be mocked
+        val prefs = mockk<Preferences>(relaxed = true)
+    
+        // use every to define what behaviour is going to be mocked
+        every { prefs.loadUserInfo() } returns (UserInfo(
+            gender = Gender.Male,
+            age = 28,weight = 80f,
+            height = 180,
+            activityLevel = ActivityLevel.Medium,
+            goalType = GoalType.KeepWeight,
+            carbRatio = 0.4f,
+            proteinRatio = 0.4f,
+            fatRatio = 0.3f))
+        
+        //we pass in the mocked prefs instead of the real one
+        calculateMealNutrientsUseCase = CalculateMealNutrientsUseCase(prefs)
+    }
 }
