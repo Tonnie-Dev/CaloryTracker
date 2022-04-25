46dc2c39dd407d9f5239a4d87cb90ef05b239e57
Tonnie <vontonnie@gmail.com>
2022-04-25 08:48:23 +0300

    Implement searchFood function of TrackerRepositoryFake class

--- app/src/androidTest/java/com/plcoding/calorytracker/repository/TrackerRepositoryFake.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/repository/TrackerRepositoryFake.kt
@@ -6,11 +6,28 @@ import com.uxstate.tracker_domain.repository.TrackerRepository
 import kotlinx.coroutines.flow.Flow
 import java.time.LocalDate
 
-class TrackerRepositoryFake: TrackerRepository {
+//implements repository interface which is handy in testing
+
+//simulates the behaviour of the real repository but this faster
+//by saving everything in memory instead of using the real one
+//which could be affected by things like the server being down
+//or connection problem which don't indicate a mistake on our code
+class TrackerRepositoryFake : TrackerRepository {
+    
+    val shouldReturnError = false
+    
+    //simulated list of TrackedFood from the database
+    private val trackedFood =
+        mutableListOf<TrackedFood>() //public to enable setting up of the list from outside
+    
+    val searchResults = listOf<TrackableFood>()
+    
+    
     override suspend fun searchFood(
         query: String, page: Int, pageSize: Int
     ): Result<List<TrackableFood>> {
-        TODO("Not yet implemented")
+        return if (shouldReturnError) Result.failure(Throwable())
+        else Result.success(searchResults)
     }
     
     override suspend fun insertTrackedFood(trackedFood: TrackedFood) {
