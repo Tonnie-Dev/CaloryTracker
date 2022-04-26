1abffd485bb60d7bb54033eb01c2ce4c988663ea
Tonnie <vontonnie@gmail.com>
2022-04-26 08:10:15 +0300

    Implement getFoodForDate function of TrackerRepositoryFake class

--- app/src/androidTest/java/com/plcoding/calorytracker/repository/TrackerRepositoryFake.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/repository/TrackerRepositoryFake.kt
@@ -4,6 +4,7 @@ import com.uxstate.tracker_domain.model.TrackableFood
 import com.uxstate.tracker_domain.model.TrackedFood
 import com.uxstate.tracker_domain.repository.TrackerRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import java.time.LocalDate
 import kotlin.random.Random
 
@@ -23,7 +24,7 @@ class TrackerRepositoryFake : TrackerRepository {
     
     val searchResults = listOf<TrackableFood>()
     
-    
+    private val getFoodsForDateFlow = MutableSharedFlow<List<TrackedFood>>(replay = 1)
     override suspend fun searchFood(
         query: String, page: Int, pageSize: Int
     ): Result<List<TrackableFood>> {
@@ -36,13 +37,18 @@ class TrackerRepositoryFake : TrackerRepository {
         //simulates the db insertion but doesn't autogenerate ids
         // so we generate a random id
         this.trackedFood.add(trackedFood.copy(id = Random.nextInt()))
+        
+        //database changed, we emit a new list of tracked foood using shared flow
+        getFoodsForDateFlow.emit(this.trackedFood)
     }
     
     override suspend fun deleteTrackedFood(trackedFood: TrackedFood) {
       this.trackedFood.remove(trackedFood)
+        //database changed, we emit a new list of tracked foood using shared flow
+        getFoodsForDateFlow.emit(this.trackedFood)
     }
     
     override fun getFoodForDate(localDate: LocalDate): Flow<List<TrackedFood>> {
-        TODO("Not yet implemented")
+   return getFoodsForDateFlow
     }
 }
