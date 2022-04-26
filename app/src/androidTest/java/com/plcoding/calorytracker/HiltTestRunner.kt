f813aa6808948f6e467893e6a6230d272f007f80
Tonnie <vontonnie@gmail.com>
2022-04-26 19:22:36 +0300

    Override newApplication() in HiltTestRunner

--- app/src/androidTest/java/com/plcoding/calorytracker/HiltTestRunner.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/HiltTestRunner.kt
@@ -1,5 +1,23 @@
 package com.plcoding.calorytracker
 
+import android.app.Application
+import android.content.Context
 import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltTestApplication
 
-class HiltTestRunner:AndroidJUnitRunner() {}
+
+/*By default this class won't be used as a test runner,
+* We need to define in our gradle the test runner to be used*/
+class HiltTestRunner:AndroidJUnitRunner() {
+    
+    //override newApplication()
+    override fun newApplication(
+        cl: ClassLoader?, className: String?, context: Context?
+    ): Application {
+       // return super.newApplication(cl, className, context)
+        
+        //instead of className, we return
+        
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
