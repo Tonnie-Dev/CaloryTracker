66c869e6566879df1d793dfaf14d223866f96045
Tonnie <vontonnie@gmail.com>
2022-04-27 08:23:41 +0300

    Define lateinit vars

--- app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
@@ -4,8 +4,16 @@ package com.plcoding.calorytracker
 
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import com.plcoding.calorytracker.repository.TrackerRepositoryFake
+import com.uxstate.core.domain.preferences.Preferences
+import com.uxstate.tracker_domain.use_cases.TrackerUseCases
+import com.uxstate.tracker_presentation.search.SearchViewModel
+import com.uxstate.tracker_presentation.tracker_overview.TrackerOverviewViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
 import org.junit.Rule
 
 @HiltAndroidTest
@@ -15,6 +23,23 @@ class TrackerOverViewE2E {
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
     
+    private lateinit var repositoryFake: TrackerRepositoryFake
+    private lateinit var useCases:TrackerUseCases
+    private lateinit var prefs:Preferences
+    
+    //viewModels for our 2 screens
+    
+    private lateinit var trackerOverviewViewModel:TrackerOverviewViewModel
+    private lateinit var searchViewModel:SearchViewModel
+    
+    //to verify nav worked
+    private lateinit var navController:NavHostController
+    
+    @Before
+    fun setUp(){
+        //injects dependencies in this test class for dagger hilt
+        hiltRule.inject()
+    }
    
     
 }
