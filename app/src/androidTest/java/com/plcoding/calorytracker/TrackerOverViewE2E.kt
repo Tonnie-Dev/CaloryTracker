4be5404894498aa1c01656e064e7745078e706e2
Tonnie <vontonnie@gmail.com>
2022-04-27 08:36:44 +0300

    Mock prefences dependencies

--- app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
@@ -8,12 +8,18 @@ import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.plcoding.calorytracker.repository.TrackerRepositoryFake
+import com.uxstate.core.domain.model.ActivityLevel
+import com.uxstate.core.domain.model.Gender
+import com.uxstate.core.domain.model.GoalType
+import com.uxstate.core.domain.model.UserInfo
 import com.uxstate.core.domain.preferences.Preferences
 import com.uxstate.tracker_domain.use_cases.TrackerUseCases
 import com.uxstate.tracker_presentation.search.SearchViewModel
 import com.uxstate.tracker_presentation.tracker_overview.TrackerOverviewViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Before
 import org.junit.Rule
 
@@ -47,7 +53,21 @@ class TrackerOverViewE2E {
     
     @Before
     fun setUp(){
-       
+    //create mockk to initialize preferences
+        
+        prefs =  mockk(relaxed = true)
+        //make prefs.loadUserInfo return a custom UserInfo
+        every { prefs.loadUserInfo() }returns UserInfo(
+            gender = Gender.Male,
+            age = 35,
+            weight = 78.0f,
+            height = 163,
+            activityLevel =ActivityLevel.Medium,
+            goalType = GoalType.LoseWeight,
+            carbRatio = 0.6f,
+            proteinRatio = 0.2f,
+            fatRatio = 0.2f
+        )
     }
    
     
