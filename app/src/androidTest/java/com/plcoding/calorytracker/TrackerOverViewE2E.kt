32725c1a66c58f816da87a0f533a994272e6e31c
Tonnie <vontonnie@gmail.com>
2022-04-27 08:13:35 +0300

    Get hilt rule

--- app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
@@ -1,6 +1,20 @@
 package com.plcoding.calorytracker
 
+
+
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 
 @HiltAndroidTest
-class TrackerOverViewE2E {}
+class TrackerOverViewE2E {
+    
+    //define hilt rule
+    @get:Rule
+    val hiltRule = HiltAndroidRule(this)
+    
+   
+    
+}
