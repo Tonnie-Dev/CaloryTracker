3cea6d66ada6150c316aa330a1829ae844521aae
Tonnie <vontonnie@gmail.com>
2022-04-28 08:21:16 +0300

    Set up the composables to test

--- app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
+++ app/src/androidTest/java/com/plcoding/calorytracker/TrackerOverViewE2E.kt
@@ -1,8 +1,17 @@
 package com.plcoding.calorytracker
 
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.plcoding.calorytracker.navigation.Route
 import com.plcoding.calorytracker.repository.TrackerRepositoryFake
 import com.uxstate.core.domain.model.ActivityLevel
 import com.uxstate.core.domain.model.Gender
@@ -10,8 +19,18 @@ import com.uxstate.core.domain.model.GoalType
 import com.uxstate.core.domain.model.UserInfo
 import com.uxstate.core.domain.preferences.Preferences
 import com.uxstate.core.domain.use_cases.FilterOutDigits
+import com.uxstate.onboarding_presentation.activity.ActivityLevelScreen
+import com.uxstate.onboarding_presentation.age.AgeScreen
+import com.uxstate.onboarding_presentation.gender.GenderScreen
+import com.uxstate.onboarding_presentation.goal.GoalScreen
+import com.uxstate.onboarding_presentation.height.HeightScreen
+import com.uxstate.onboarding_presentation.nutrients_goal.NutrientsScreen
+import com.uxstate.onboarding_presentation.weight.WeightScreen
+import com.uxstate.onboarding_presentation.welcome.WelcomeScreen
 import com.uxstate.tracker_domain.use_cases.*
+import com.uxstate.tracker_presentation.search.SearchScreen
 import com.uxstate.tracker_presentation.search.SearchViewModel
+import com.uxstate.tracker_presentation.tracker_overview.TrackerOverviewScreen
 import com.uxstate.tracker_presentation.tracker_overview.TrackerOverviewViewModel
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
@@ -82,6 +101,61 @@ class TrackerOverViewE2E {
             TrackerOverviewViewModel(trackerUseCases = useCases, prefs = prefs)
         searchViewModel =
             SearchViewModel(trackerUseCases = useCases, filterOutDigits = FilterOutDigits())
+        
+        /*set up the composables that we want to test i.e. don't incude onboarding
+        * for every test case we will need to define the compose rule*/
+        
+        composeRule.setContent {
+            
+            val scaffoldState = rememberScaffoldState()
+            Scaffold(modifier = Modifier.fillMaxSize(), scaffoldState = scaffoldState) {
+                NavHost(
+                    navController = navController,
+                    startDestination =  Route.TRACKER_OVERVIEW,
+                    builder = {
+                
+                       //Screen 1
+                        composable(route = Route.TRACKER_OVERVIEW, content = {
+                    
+                            TrackerOverviewScreen(onNavigateToSearch = { mealName, day, month, year ->
+                                navController.navigate(
+                                    Route.SEARCH +
+                                            "/$mealName"+
+                                            "/$day" +
+                                            "/$month"+
+                                            "/$year"
+                                )
+                            })
+                        })
+                        
+                        //Screen 2
+                        composable(route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument(name = "mealName") { type = NavType.StringType },
+                                navArgument(name = "dayOfMonth") { type = NavType.IntType },
+                                navArgument(name = "month") { type = NavType.IntType },
+                                navArgument(name = "year") { type = NavType.IntType }),
+                            content = {
+                        
+                                val mealName = it.arguments?.getString("mealName")!!
+                                val dayOfMonth = it.arguments?.getInt("dayOfMonth")!!
+                                val month = it.arguments?.getInt("month")!!
+                                val year = it.arguments?.getInt("year")!!
+                                SearchScreen(
+                                    scaffoldState = scaffoldState,
+                                    mealName = mealName,
+                                    dayOfMonth = dayOfMonth,
+                                    month = month,
+                                    year = year,
+                                    onNavigateUp = { navController.navigateUp() }
+                                )
+                            })
+                
+                
+                    })
+            }
+        
+        }
     }
     
     
